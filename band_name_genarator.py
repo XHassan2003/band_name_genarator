59dbf380eb1230540aeafd4144d4083ca8975dcd
XHassan2003 <132691813+XHassan2003@users.noreply.github.com>
2023-09-26 19:40:09 +0500

    Add files via upload

---
+++ band_name_genarator.py
@@ -0,0 +1,16 @@
+#Band name Generator:
+
+print("Welcome to Band name Generator!")
+city = input("Enter your city that you grew up in:\n")
+food = input("Enter you favourite thing to eat:\n")
+print("Your band name could be", city, food)
+
+
+
+
+  
+
+    
+
+
+
